2b2f947d80919a18f3a8260280285653148c7ed7
Santiago Alvarez Soto <alvarezsoto755@hotmail.com>
2019-02-22 22:05:23 -0500

    predice pecimamente

--- server.R
+++ server.R
@@ -1,25 +1,48 @@
 library(shiny)
+library(keras)
 
 server <- function(input, output) {
   
-  pred <- 0.5
+  # Cargar Modelo Guardado
+  model <- load_model_hdf5("./model.hdf5")
+  model %>% compile(
+    loss = "binary_crossentropy",
+    optimizer = optimizer_rmsprop(lr = 2e-5),
+    metrics = c("accuracy")
+  )
+  
+  
+  observeEvent(input$file,{
+    
+    # Obtener y preparar imagen
+    test_img <- image_load(input$file$datapath, target_size = c(32, 32))
+    x = image_to_array(test_img)
+    x <- array_reshape(x, c(1, dim(x)))
+    x <- imagenet_preprocess_input(x)
+    
+    # Calcular prediccion de la imagen x
+    preds <- model %>% predict_classes(x)
+    print(preds)
+    
+    #Mostrar el resultado basándose en la condición
+    output$result <-
+      
+      if (preds < 0.5) {
+        renderText("Tiene gafas")
+      }
+      else {
+        renderText("No tiene gafas")
+      }
+  })
+  
   output$contents <- renderTable({
     
     req(input$file)
     
     #Ruta donde se encuentra el archivo cargado para hacerle la predicción
-    input$file$datapath
+    print(input$file$datapath)
     
     input$file
     
   })
-  
-  #Mostrar el resultado basándose en la condición
-  output$result <-
-    if (pred > 0.5) {
-      renderText("Tiene gafas")
-    }
-    else {
-      renderText("No tiene gafas")
-    }
 }
